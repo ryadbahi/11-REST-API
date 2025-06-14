fa205a83859d997b991d380b8ead955aa03aeb9e
ryad <ryadbahi@gmail.com>
2025-06-14 22:16:34 +0100

    update server port to 5000

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.delete("/users/:id", async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
